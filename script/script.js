fd4aec49901d70070ce6ebee4b31b66e51e24320
Edwike12 <nyaunchoedwike@gmail.com>
2021-10-18 10:17:35 +0300

    gender function declaration

--- script/script.js
+++ script/script.js
@@ -4,4 +4,13 @@ let yearOfBirth=Document.getElementById("year-input").value;
 let monthOfBirth=Number(document.getElementById("day-input").value);
 let dayOfBirth=Number(document.getElementById("day-input").value);
 let genders=Document.getElementById("gender")
+
+//declare function for selecting gender
+function getGender(){
+    
+  var genders = document.getElementById("gender");
+  var gender = genders.options[genders.selectedIndex].text;
 }
+}
+let myGenderValue = getGender();
+console.log(myGenderValue);
