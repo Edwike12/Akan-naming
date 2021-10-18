d684a260b9d22758a067bf4b33376adf04e15ab7
Edwike12 <nyaunchoedwike@gmail.com>
2021-10-18 11:55:57 +0300

    formulae for calculating day of the week

--- script/script.js
+++ script/script.js
@@ -54,4 +54,9 @@ var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
 var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
 
+//formulae for calculating birthDay from days of the week
+var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+  var dayOfBirth = Math.floor(dayOfBirth);
+
+
 
