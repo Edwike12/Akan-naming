93fa7002a03065b8096d27e58377a5493db172ad
Edwike12 <nyaunchoedwike@gmail.com>
2021-10-18 09:38:03 +0300

    declared variable

---
+++ script/script.js
@@ -0,0 +1,7 @@
+
+function getAkanName () {
+let yearOfBirth=Document.getElementById("year-input").value;
+let monthOfBirth=Number(document.getElementById("day-input").value);
+let dayOfBirth=Number(document.getElementById("day-input").value);
+let genders=Document.getElementById("gender")
+}
